84e85ce82ff3ec17fd7ddf9a37c03df965e982f5
techno2610 <officialtechno26@gmail.com>
2025-07-20 12:16:52 +0530

    changed the match value

--- src/components/CameraScreen.tsx
+++ src/components/CameraScreen.tsx
@@ -201,7 +201,7 @@ useEffect(() => {
 
         console.log("Match confidence:", maxVal);
 
-        if (maxVal >= 0.5 && !isPlayingVideo) {
+        if (maxVal >= 0.6 && !isPlayingVideo) {
   console.log("🎯 Target detected!");
 
   // Stop detection
